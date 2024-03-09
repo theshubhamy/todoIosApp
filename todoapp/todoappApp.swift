b22183d922410be3bd9e25c08ed715dacd383bb8
Shubham <theshubham.dev@gmail.com>
2024-03-09 16:36:32 +0530

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by shubham kumar on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
