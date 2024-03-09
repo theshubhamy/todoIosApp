4e55b3c4c8379c841b645c3407b388dd5e52f00e
Shubham <theshubham.dev@gmail.com>
2024-03-09 17:04:22 +0530

    setup  with firebase

--- todoapp/todoappApp.swift
+++ todoapp/todoappApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
+
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    
+//    return true
+//  }
+//}
 @main
 struct todoappApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+
